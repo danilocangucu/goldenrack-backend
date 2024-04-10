12e6b46413589974fed497f0b6ff384106237585
Danilo Canguçu <daniloccangucu@gmail.com>
2024-04-10 21:36:46 +0300

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 import recordsRouter from "./routers/recordsRouter";
@@ -8,6 +9,8 @@ import usersRouter from "./routers/usersRouter";
 const baseUrl = "/api/v1";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(`${baseUrl}/records`, recordsRouter);
